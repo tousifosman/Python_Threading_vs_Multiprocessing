a6482e17c7e8f69f1dccbf0c85226c3dbc2de11b
Tousif Osman <tousifosman@gmail.com>
2017-09-17 17:05:10 +0600

    Updated

--- ThreadingVsMultiprocessing.py
+++ ThreadingVsMultiprocessing.py
@@ -1,17 +1,18 @@
 import sched, time, threading, multiprocessing
 
 stopThread = False
-stopProcess = False
 threadCounts = []
 processCounts = []
 
 s = sched.scheduler(time.time, time.sleep)
 
 #%% Handle Thread
-
 def thread_handle(args):
+    '''
+    Used global stopThread variable to stop all threads at the same time
+    '''
     global threadCounts
-    print ('In Thread - %d\n' % args),
+    print ('In Thread - %d\n' % args, end='')
     counter = 0
     while not stopThread:
         counter += 1
@@ -27,60 +28,87 @@ def process_thread():
         t = threading.Thread(target=thread_handle, args=[i])
         t.start()
         threads.append(t)
-        
+
 def stop_thread():
     global stopThread
-    global threadCounts
-    global thread
-
     stopThread = True
 
-    for t in threads:
-        t.join()
-        
-    print ('Process Counts -', sum(threadCounts))
 
 #%% Handle Process
-
-def process_handle(args):
-    global stopProcess    
-    global processCounts
-    print ('In Process - %d' % args)
-    counter = 0;
+def process_handle(args, processCounts):
+    '''
+    As process has separate memory space stopProcess variable is not share 
+    among all the processes. Therefore, a single schedular cannot be used to 
+    stop all processes at the same time.
+    '''
+    global stopProcess
     
-    while not False:
+    stopProcess = False
+    
+    print ('In Process - %d' % args)
+            
+            
+    s = sched.scheduler(time.time, time.sleep)
+    
+    def stop_process():
+        global stopProcess
+        stopProcess = True
+    
+    s.enter(5, 2, stop_process, ())
+    threading.Thread(target=lambda : s.run()).start()
+    
+    counter = 0;
+    while not stopProcess:
         counter += 1
-        print(stopProcess)
-
+    
     processCounts.append(counter)
 
 processes = []
 def process_process():
     global processes
-    print ('Processing Process ......')
+    global processCounts
+    
+    print ('\nProcessing Process ......')
+    
+    processCounts = multiprocessing.Manager().list()
 
     for i in range(5):
-        p = multiprocessing.Process(target=process_handle, args=[i])
+        p = multiprocessing.Process(target=process_handle, args=(i, processCounts))
         p.start()
         processes.append(p)
-        
-def stop_process():
-    global stopProcess
+
+#%% End Calculations      
+def stop():
+    global threadCounts
+    global threads
     global processCounts
     global processes
     
-    print('In Stop process -', stopProcess)
-    stopProcess = True
+    for t in threads:
+        t.join()
+        
     
     for p in processes:
         p.join()
-        
-    print ('Process Counts -', sum(processCounts))
+    
+    totalThreadCounts = sum(threadCounts)
+    totalProcessCounts = sum(processCounts)
+    
+    print ('\nThread Counts -', totalThreadCounts )
+    print ('\nProcess Counts -', totalProcessCounts, end='\n\n')
+    
+    if totalThreadCounts > totalProcessCounts:
+        print ('Thread is faster')
+    elif totalThreadCounts < totalProcessCounts:
+        print ('Process is faster')
+    else:
+        print ('Its a Draw')
 
-#s.enter(0, 1, process_thread, ())
-s.enter(0, 1, process_process, ())
-#s.enter(5, 2, stop_thread, ())
-s.enter(5, 2, stop_process, ())
+#%% Schedule Program
+s.enter(0, 1, process_thread, ())
+s.enter(5, 1, stop_thread, ())
+s.enter(6, 2, process_process, ())
+s.enter(6, 1, stop, ())
 s.run()
 
         
